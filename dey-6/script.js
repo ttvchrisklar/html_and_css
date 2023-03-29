87d985668fc0ffa8a65697e2edc71fc9d42a3d2b
ttvchrisklar <106084524+ttvchrisklar@users.noreply.github.com>
2023-03-29 12:45:54 +0200

    ok

--- dey-6/script.js
+++ dey-6/script.js
@@ -1,25 +1,31 @@
 var div1 = document.getElementById("1"),
     div2 = document.getElementById("2"),
     div3 = document.getElementById("3"),
-    div4 = document.getElementById("4");
+    div4 = document.getElementById("4"),
+    curentStorypart = 0;
+const story = {
+    0: ``,
+    1: `wellcome to the danger sone`,
+    2: `"you are curently the only one seeing this" the comander told you, as you wer looking at some clasefide documents.`,
+    3: `'why am I looking at theas documents comander, my rank is way to low to be alowd to read this?' you ask the comander.`,
+    4: `"the reason you are looking at theas documents is becuse you are the mote cualifide at the moment" he tels you`,
+};
 
 div1.innerHTML = `<p id="text1" class="div-text"></p>`;
 var textEntry = document.getElementById("text1");
 function divclickd(presdDiv) {
     switch (presdDiv.id) {
         case "1":
-            console.log("sucses");
-
             break;
         case "2":
-            console.log("sucses");
+            curentStorypart--;
+            textEntry.innerHTML = story[curentStorypart];
             break;
         case "3":
-            console.log("sucses");
             break;
         case "4":
-            console.log("sucses");
-            textEntry.innerHTML = `hello so is this working?<br>`;
+            curentStorypart++;
+            textEntry.innerHTML = story[curentStorypart];
             break;
         default:
             console.error(`${presdDiv.id} is not a div`);
